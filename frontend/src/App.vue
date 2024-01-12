a43530c1798bce4d0b70c335751cd94f74272ed2
MIGUEL MONCADA <mma106@inlumine.ual.es>
2024-01-12 18:19:02 +0100

    Grade 3

---
+++ frontend/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-app>
+    
+    <v-navigation-drawer expand-on-hover rail permanent v-model="showNavigation">
+      
+      <v-list density="compact" nav>
+        <template v-for="item in navigation" :key="item.title">
+          <v-list-item :href="item.href" :prepend-icon="item.icon" :title="item.title" exact v-show="checkIfInRole(user, item.roles)"/>
+        </template>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list v-if="user.username">
+    <v-list-item>
+      <v-avatar>
+        <v-img v-for="role in user.roles" :key="role" :src="getRoleIcon(role)" />
+      </v-avatar>
+      <v-list-item-content >
+            <template v-if="showNavigation">
+              <v-list-item
+          :title="user.username"
+        >
+        </v-list-item>
+            </template>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+
+      <v-list density="compact" nav>
+        <v-list-item key="Login" @click="prepareToLogin" prepend-icon="mdi-login" title="Login" exact v-if="!user.username"/>
+        <v-list-item key="Logout" @click="logoutConfirmation = true" prepend-icon="mdi-logout" title="Logout" exact v-if="user.username"/>
+      </v-list>
+
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view :user="user"></router-view>
+    </v-main>
+
+    <v-dialog v-model="loginDialog" width="25em">
+      <Login @cancel="loginDialog = false" @loginFailed="loginDialog = false" @loginSuccess="login"/>
+    </v-dialog>
+
+    <v-dialog v-model="logoutConfirmation" width="auto">
+      <ConfirmationDialog :question="'Are you sure to logout?'" @ok="logout" @cancel="logoutConfirmation = false"/>
+    </v-dialog>
+
+  </v-app>
+</template>
+
+<script>
+import common from './mixins/common'
+import Login from './components/Login.vue'
+import ConfirmationDialog from './components/ConfirmationDialog.vue'
+
+export default {
+  name: 'App',
+  components: { Login, ConfirmationDialog },
+  mixins: [ common ],
+  data() {
+    return {
+      navigation: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', href: '#/' },
+          { title: 'Persons', icon: 'mdi-account-multiple', href: '#/persons', roles: [ 0, 1 ] },
+          { title: 'Projects', icon: 'mdi-sitemap-outline', href: '#/projects' },
+          { title: 'Tasks', icon:'mdi-calendar-check-outline' , href: '#/tasks' , roles: [ 0, 1 ] },
+          { title: 'Chat', icon: 'mdi-chat-processing-outline', href: '#/chat' }
+      ],
+      showNavigation: false,
+      user: {},
+      loginDialog: false,
+      logoutConfirmation: false
+    }
+  },
+  methods: {
+    getRoleIcon(role) {
+      switch (role) {
+        case 0:
+          return require('./assets/adminicon.png');
+        case 1:
+          return require('./assets/user.png');
+        // Puedes añadir más casos según los roles que tengas
+        default:
+          return require('./assets/vue.png');
+      }
+    },
+    setUser(data) {
+      this.loginDialog = false
+      Object.keys(this.user).forEach(key => delete this.user[key])
+      Object.assign(this.user, data)
+      this.$router.push('/')
+      this.showNavigation = true
+    },
+    logout() {
+      this.logoutConfirmation = false
+      this.showNavigation = false
+      fetch('/auth', { method: 'DELETE' })
+      .then(res => res.json())
+      .then(data => this.setUser(data))
+    },
+    prepareToLogin() {
+      this.showNavigation = false
+      this.loginDialog = true
+    },
+    login(data) {
+      this.loginDialog = false
+      this.setUser(data)
+    }
+  },
+  mounted() {
+    fetch('/auth', { method: 'GET' })
+    .then(res => res.json())
+    .then(data => this.setUser(data))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  margin: 10px 30px;
+}
+</style>
---
+++ frontend/src/components/TasksLister.vue
@@ -0,0 +1,191 @@
+<template>
+  <div v-if="checkIfInRole(user, [ 0, 1 ])">
+    <v-card variant="text">
+      <v-card-title>Tasks</v-card-title>
+      <v-card-subtitle>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-text-field variant="solo" label="Search" v-model="search" @input="retrieve"></v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-select v-model="status" label="Status" :items="[ { value: 0, title: 'preparation' }, { value: 1, title: 'pending' }, { value: 2, title: 'in tests' }, { value: 3, title: 'completed' } ]" chips multiple @update:modelValue="retrieve">
+              </v-select>
+            </v-col>
+            <v-col cols="2">
+              <v-text-field variant="solo" type="number" label="Skip" v-model="skip" @input="retrieve"></v-text-field>
+            </v-col>
+            <v-col cols="2">
+              <v-text-field variant="solo" type="number" label="Limit" v-model="limit" @input="retrieve"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-subtitle>
+      <v-card-text>
+        <v-table density="compact" hover>
+          <thead>
+            <tr>
+              <th class="text-left">
+                Name
+              </th>
+              <th class="text-left">
+                Status
+              </th>
+              <th class="text-left">
+                Project
+              </th>
+              <th class="text-left">
+                Workers
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(task, index) in tasks" :key="index" @click="checkIfInRole(user, [ 1 ]) && click(task)">
+              <td>{{ task.name }}</td>
+              <td><v-chip :color="[ 'orange darken-3', 'blue darken-2', 'yellow darken-3', 'success' ][task.status]">
+  {{ ['Preparation', 'Pending', 'In tests', 'Completed'][task.status] }}
+</v-chip></td>
+              <td>
+                <!--<v-chip :color="project.color" v-for="(project, pindex) in projects" :key="pindex" 
+                @update:model-value="modify(task)"
+                @click.stop="">
+                  {{ project.shortcut}}
+                </v-chip>-->
+                <!--<v-chip-group>
+                    <v-chip
+                      v-for="(project, pindex) in projects"
+                      value: project._id
+                      :key="pindex"
+                      :color="project.color"                   
+                      @click="() => modify2(task, project)"
+                      >
+                      {{ project.shortcut }}
+                    </v-chip>
+                </v-chip-group>-->
+                <v-select  
+                  v-model="task.project_id"
+                  label="Project"
+                  :items= "projects.map(project => ({ value: project._id, title: project.shortcut, props: { subtitle: project.name + ' ' + new Date(project.startDate).toLocaleDateString() } }))"
+                  variant="solo"
+                  @click.stop=""
+                  @update:model-value="modify(task)"                  
+                ></v-select>
+              </td>
+              <td>
+                <v-chip v-for="(person, pindex) in task.workers" :key="pindex">
+                  {{ person.firstName + ' ' + person.lastName }}
+                </v-chip>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn variant="elevated" color="success" @click="add" v-if="checkIfInRole(user, [ 1 ])">Add</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-dialog v-model="editor" width="50%">
+      <TaskEditor :id="id" @dataChanged="retrieve" @cancel="cancel"/>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import common from '../mixins/common'
+import TaskEditor from './TaskEditor.vue'
+
+export default {
+  name: 'TasksLister',
+  components: { TaskEditor },
+  props: [ 'user' ],
+  mixins: [ common ],
+  methods: {
+    
+    retrieve() {
+      this.id = null
+      this.editor = false
+      fetch('/task?search=' + this.search + '&status=' + JSON.stringify(this.status) + '&skip=' + this.skip + '&limit=' + this.limit, {
+        method: 'GET' })
+        .then((res) => {
+          res.json()
+            .then((data) => {
+              this.tasks = data
+            })
+            .catch(err => console.error(err.message))
+        })
+        .catch(err => console.error(err.message))
+    },
+    add() {
+      this.id = null
+      this.editor = true
+    },
+    click(row) {
+      this.id = row._id
+      this.editor = true
+    },
+    cancel() {
+      this.id = null
+      this.editor = false
+    },
+
+    modify(task) {
+      task.workers=[]
+      fetch('/task?_id=' + task._id, {
+          method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ project_id: task.project_id, workers:task.workers }) })
+        .then((res) => {
+          res.json()
+            .then(() => {
+              this.$emit('dataChanged')
+            })
+            .catch(err => console.error(err.message))
+        })
+        .catch(err => console.error(err.message))
+    },
+
+    modify2(task, project) {
+      task.workers=[]
+      fetch('/task?_id=' + task._id, {
+          method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ project_id: project.project_id, workers:task.workers }) })
+        .then((res) => {
+          res.json()
+            .then(() => {
+              this.$emit('dataChanged')
+            })
+            .catch(err => console.error(err.message))
+        })
+        .catch(err => console.error(err.message))
+    },
+
+    modifyTaskWithProject(task, project) {
+    // Llama a tu función modify con la tarea y el proyecto como argumentos
+    this.modify(task, project);
+  },
+    
+  },
+  data() {
+    return {
+      editor: false,
+      tasks: [],
+      id: null,
+      search: '',
+      status: [ 0, 1, 2, 3 ],
+      skip: 0,
+      limit: 10,
+      projects: [],
+      workers: []
+    }
+  },
+  mounted() {
+    fetch('/project?limit=1000', { method: 'GET'})
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    this.retrieve()
+  }
+
+}
+</script>
